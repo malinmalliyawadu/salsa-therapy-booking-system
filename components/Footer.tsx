b68e695b18673ffede28d71430fd8a246b81ae3a
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2021-11-10 00:02:52 +1300

    Just basic mock

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-gray-600 flex justify-center text-white py-3">
+      Salsa Therapy {new Date().getFullYear()} &copy;
+    </footer>
+  );
+};
