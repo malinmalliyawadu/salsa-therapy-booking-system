2965c34b59b1e8b8c1e27d4f2cf76a9876a03652
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2021-11-11 13:49:10 +1300

    Move some public stuff out of secrets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,9 @@ import "firebase/firestore";
 
 const clientCredentials = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  authDomain: "salsa-therapy-booking-system.firebaseapp.com",
+  projectId: "salsa-therapy-booking-system",
+  storageBucket: "salsa-therapy-booking-system.appspot.com",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   databaseURL:
