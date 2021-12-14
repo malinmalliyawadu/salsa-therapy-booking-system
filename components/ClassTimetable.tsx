e5f335172c32e8bda4f92c5e08f9b6e9bef97160
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2021-12-15 12:57:15 +1300

    Fix prop

--- components/ClassTimetable.tsx
+++ components/ClassTimetable.tsx
@@ -11,7 +11,7 @@ import { Loading } from "./Loading";
 
 interface ClassTimetableProps {
   onRowClick: (danceClass: DanceClass) => void;
-  selectedClass: DanceClass;
+  selectedClass?: DanceClass;
 }
 
 export const ClassTimetable: React.FC<ClassTimetableProps> = ({
