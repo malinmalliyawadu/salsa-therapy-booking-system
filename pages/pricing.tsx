c4a87896dde1fc1f8e5e721ddb35bc7096f44dba
Maisie <maisie.embleton@sharesight.co.nz>
2022-02-10 00:19:34 +1300

    Updating the Pricing + Stripe IDs

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,19 +1,22 @@
-import { getAuth, User } from "firebase/auth";
-import { NextPage } from "next";
-import { useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { Button } from "../components/Button";
-import { ButtonLink } from "../components/ButtonLink";
-import { PriceCard } from "../components/PriceCard";
-import { StripeForm } from "../components/StripeForm";
+import { getAuth, User } from 'firebase/auth';
+import { NextPage } from 'next';
+import { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Button } from '../components/Button';
+import { ButtonLink } from '../components/ButtonLink';
+import { PriceCard } from '../components/PriceCard';
+import { StripeForm } from '../components/StripeForm';
 import {
+  CasualClassStudent,
   DameDos,
+  DameDosStudent,
   DameTres,
   DameUna,
   DileQueSi,
-  SpecialOfferTenTrip,
-  TenTripStudentPass,
-} from "../constants/stripeProductIds";
+  FiveTripPass,
+  TenTripPass,
+  TenTripPassStudent,
+} from '../constants/stripeProductIds';
 
 const StripeCallToAction: React.FC<{
   productId: string;
@@ -31,7 +34,7 @@ const StripeCallToAction: React.FC<{
           }}
         >
           <Button type="submit" disabled={submitLoading} className="w-full">
-            {submitLoading ? "PLEASE WAIT..." : "BUY NOW"}
+            {submitLoading ? 'PLEASE WAIT...' : 'BUY NOW'}
           </Button>
         </StripeForm>
       ) : (
@@ -43,32 +46,29 @@ const StripeCallToAction: React.FC<{
   );
 };
 
+// TODO: Add pricing info to firebase
+
 const Pricing: NextPage = () => {
   const [submitLoading, setSubmitLoading] = useState(false);
   const [user, userLoading, userError] = useAuthState(getAuth());
 
   return (
     <div className="m-6">
-      <h1 className="font-bold text-6xl mb-2 tracking-tight">
-        Membership Options
-      </h1>
+      <h1 className="font-bold text-6xl mb-2 tracking-tight">Membership Options</h1>
 
       <div className="max-w-full mx-auto my-12">
         <div className="grid grid-cols-4">
           <PriceCard
-            name={"Dáme Una"}
-            description={"$18 per week"}
+            name={'Dáme Una'}
+            description={'$18 per week'}
             price={180}
             headerBg="bg-green-300"
             points={[
-              "One class per week cancel anytime",
-              "Choose 1 class",
-              "Any style and level",
-              "See Term Dates",
-              "Includes 2 FREE SALSA Practica passes for new students only",
-              "Buy and book",
-              "Booking in advance is essential",
-              "Valid for 2 months",
+              'One class per week',
+              'Any style and level',
+              'Includes 2 FREE Salsa Practica passes',
+              'Contact for student discount!',
+              'Valid within the 10 week term!',
             ]}
             callToAction={
               <StripeCallToAction
@@ -81,18 +81,15 @@ const Pricing: NextPage = () => {
           />
 
           <PriceCard
-            name={"Dáme Dos"}
-            description={"$27 per week"}
-            price={270}
+            name={'Dáme Dos'}
+            description={'$27.5 per week'}
+            price={275}
             headerBg="bg-blue-300"
             points={[
-              "Two classes per week  cancel anytime",
-              "Any two classes",
-              "Any style and level",
-              "See Term Dates",
-              "1 FREE SALSA Practica passes",
-              "Book ahead",
-              "Valid for 2 months",
+              'Two classes per week',
+              'Any classes, style and level',
+              '1 FREE Salsa Practica passes',
+              'Valid within the 10 week term!',
             ]}
             callToAction={
               <StripeCallToAction
@@ -105,19 +102,16 @@ const Pricing: NextPage = () => {
           />
 
           <PriceCard
-            name={"Dáme Tres"}
-            description={"$32 per week"}
-            price={320}
+            name={'Dáme Tres'}
+            description={'$32.5 per week'}
+            price={325}
             headerBg="bg-yellow-300"
             points={[
-              "Three Classes per week cancel anytime",
-              "PRIME LOCATION",
-              "Any 3 classes",
-              "Any style and level",
-              "1 FREE SALSA Practica passes",
-              "$10 off Private",
-              "Book ahead",
-              "Valid for 2 months",
+              'Three classes per week',
+              'Any classes, style and level',
+              '1 FREE Salsa Practica passes',
+              '$10 off Private lessons',
+              'Valid within the 10 week term!',
             ]}
             callToAction={
               <StripeCallToAction
@@ -127,21 +121,21 @@ const Pricing: NextPage = () => {
                 setSubmitLoading={setSubmitLoading}
               />
             }
+            isMostPopular={true}
           />
 
           <PriceCard
-            name={"Díle que si"}
-            description={"$37 per week"}
-            price={370}
+            name={'Díle Que Si'}
+            description={'$37.5 per week'}
+            price={375}
             headerBg="bg-red-300"
             points={[
-              "UNLIMITED DANCE CLASS (based on 6 month membership)",
-              "PRIME LOCATION",
-              "All dance classes",
-              "Any style and level",
-              "$10 off Private",
-              "5% OFF Performance training",
-              "2 Party Passes SLP",
+              'UNLIMITED classes in a term',
+              'Any classes, styles and levels',
+              '$10 off Private lessons',
+              '10% OFF Performance training',
+              '2 FREE practice passes',
+              'Valid within the 10 week term!',
             ]}
             callToAction={
               <StripeCallToAction
@@ -154,10 +148,10 @@ const Pricing: NextPage = () => {
           />
 
           <PriceCard
-            name={"Casual rate"}
-            description={"Book and drop-in"}
+            name={'Casual Rate'}
+            description={'Book and drop-in'}
             price={20}
-            points={["1 casual class", "Any level or style", "Buy and book"]}
+            points={['1 casual class', 'Any level or style', 'Buy and book']}
             callToAction={
               <ButtonLink href="/" className="block">
                 BOOK A CLASS
@@ -166,37 +160,13 @@ const Pricing: NextPage = () => {
           />
 
           <PriceCard
-            name={"Special offer 10 trip"}
-            description={"Save $20 with this package"}
-            price={160}
-            points={[
-              "Choose ANY style and ANY level",
-              "Book ahead",
-              "Valid for 2 months",
-            ]}
+            name={'5 Trip Pass'}
+            description={'-'}
+            price={20}
+            points={['Choose any style and any level', 'Valid for 3 months from date of purchase']}
             callToAction={
               <StripeCallToAction
-                productId={SpecialOfferTenTrip}
-                user={user}
-                submitLoading={submitLoading}
-                setSubmitLoading={setSubmitLoading}
-              />
-            }
-            isMostPopular={true}
-          />
-
-          <PriceCard
-            name={"10 trip student pass"}
-            description={"-"}
-            price={136}
-            points={[
-              "Choose ANY style and ANY level",
-              "Book ahead",
-              "Valid for 2 months",
-            ]}
-            callToAction={
-              <StripeCallToAction
-                productId={TenTripStudentPass}
+                productId={FiveTripPass}
                 user={user}
                 submitLoading={submitLoading}
                 setSubmitLoading={setSubmitLoading}
@@ -205,23 +175,87 @@ const Pricing: NextPage = () => {
           />
 
           <PriceCard
-            name={"Private 1-on-1"}
-            description={"-"}
+            name={'10 Trip Pass'}
+            description={'-'}
+            price={138}
+            points={['Choose any style and any level', 'Valid for 3 months from date of purchase']}
+            callToAction={
+              <StripeCallToAction
+                productId={TenTripPass}
+                user={user}
+                submitLoading={submitLoading}
+                setSubmitLoading={setSubmitLoading}
+              />
+            }
+          />
+
+          <PriceCard
+            name={'Private 1-on-1'}
+            description={'-'}
             price={20}
             points={[
-              "1 Student w/ 1 Teacher for $45 (per 30mins)",
-              "2 Students w/ 1 Teacher for $45 (per 30mins)",
-              "2 Couples w/ 1 Teacher for $100 ($25 each)",
-              "For x2 Teachers, add $45 per 30mins",
-              "Studio hire... FREE",
+              '1 Student w/ 1 Teacher for $45 (per 30mins)',
+              '2 Students w/ 1 Teacher for $45 (per 30mins)',
+              '2 Couples w/ 1 Teacher for $100 ($25 each)',
+              'For x2 Teachers, add $45 per 30mins',
+              'Studio hire... FREE',
             ]}
             callToAction={
-              <ButtonLink
-                href="mailto:lily@salsatherapy.co.nz"
-                className="block"
-              >
+              <ButtonLink href="mailto:lily@salsatherapy.co.nz" className="block">
                 ENQUIRE
               </ButtonLink>
+            }
+          />
+
+          {/* TODO: have a 'Student pricing:' heading */}
+
+          <PriceCard
+            name={'Casual Class - Student Special'}
+            description={'-'}
+            price={15}
+            points={['Choose any style and any level', 'Great Flexibility']}
+            callToAction={
+              <StripeCallToAction
+                productId={CasualClassStudent}
+                user={user}
+                submitLoading={submitLoading}
+                setSubmitLoading={setSubmitLoading}
+              />
+            }
+          />
+
+          <PriceCard
+            name={'10 Trip Student Pass'}
+            description={'-'}
+            price={138}
+            points={['Choose any style and any level', 'Valid for 3 months from date of purchase']}
+            callToAction={
+              <StripeCallToAction
+                productId={TenTripPassStudent}
+                user={user}
+                submitLoading={submitLoading}
+                setSubmitLoading={setSubmitLoading}
+              />
+            }
+          />
+
+          <PriceCard
+            name={'Dáme Dos - Student Special'}
+            description={'Save $20 with this package'}
+            price={160}
+            points={[
+              'Two classes per week',
+              'Any classes, style and level',
+              '1 FREE Salsa Practica passes',
+              'Valid within the 10 week term!',
+            ]}
+            callToAction={
+              <StripeCallToAction
+                productId={DameDosStudent}
+                user={user}
+                submitLoading={submitLoading}
+                setSubmitLoading={setSubmitLoading}
+              />
             }
           />
         </div>
@@ -230,21 +264,13 @@ const Pricing: NextPage = () => {
           <h2 className="font-bold mb-4 text-xl">Terms</h2>
           <ul className="list-disc ml-6">
             <li>
-              We do not offer refunds or partial refunds for unused passes
-              unless injury or serious illness occur and students will need to
-              present proof medical statements.
+              We do not offer refunds or partial refunds for unused passes unless injury or serious
+              illness occur and students will need to present proof medical statements.
             </li>
-            <li>
-              Passes can be shared once only with a partner or best friend.
-            </li>
+            <li>Passes can be shared once only with a partner or best friend.</li>
             <li>Passes and memberships exclude special events.</li>
-            <li>
-              We can only pause membership during lockdowns and unforeseen
-              circumstances.
-            </li>
-            <li>
-              Student ID is to be presented at the reception for student deals.
-            </li>
+            <li>We can only pause membership during lockdowns and unforeseen circumstances.</li>
+            <li>Student ID is to be presented at the reception for student deals.</li>
             <li>All passes activate on the purchase date.</li>
           </ul>
         </div>
