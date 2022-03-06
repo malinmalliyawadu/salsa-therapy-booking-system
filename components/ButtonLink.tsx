f1a73eddbae37f3a3d2ed6180bb4b37a5b90b1a9
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2022-03-07 01:12:23 +1300

    Fix underline on ButtonLink

--- components/ButtonLink.tsx
+++ components/ButtonLink.tsx
@@ -13,14 +13,14 @@ export const ButtonLink: React.FC<ButtonLinkProps> = ({
     return appearance === 'primary' ? (
         <a
             href={href}
-            className={`shadow-lg shadow-indigo-500/50 transition-all transform hover:shadow-lg py-3 px-4 rounded-md hover:scale-105 bg-purple-500 hover:bg-purple-600 text-white  ${className}`}
+            className={`hover:no-underline shadow-lg shadow-indigo-500/50 transition-all transform hover:shadow-lg py-3 px-4 rounded-md hover:scale-105 bg-purple-500 hover:bg-purple-600 text-white  ${className}`}
         >
             {children}
         </a>
     ) : (
         <a
             href={href}
-            className={`transition-all transform hover:shadow-lg py-3 px-4 rounded-md hover:scale-105 outline outline-purple-500 hover:bg-purple-600 text-purple-500 hover:text-white ${className}`}
+            className={`hover:no-underline transition-all transform hover:shadow-lg py-3 px-4 rounded-md hover:scale-105 outline outline-purple-500 hover:bg-purple-600 text-purple-500 hover:text-white ${className}`}
         >
             {children}
         </a>
