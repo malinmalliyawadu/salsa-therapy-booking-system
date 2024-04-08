af30b5b1b929bd2fe7b4e377ecac49f6e9b4c93c
embletmais <embletmais@myvuw.ac.nz>
2024-04-08 17:32:11 +0800

    Linking to new salsa therapy site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,30 +7,6 @@ import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 import { Layout } from '../components/Layout';
 import { useRouter } from 'next/dist/client/router';
 
-const clientCredentials = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: 'salsa-therapy-booking-system.firebaseapp.com',
-    projectId: 'salsa-therapy-booking-system',
-    storageBucket: 'salsa-therapy-booking-system.appspot.com',
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    databaseURL:
-        'https://salsa-therapy-booking-system-default-rtdb.asia-southeast1.firebasedatabase.app',
-};
-
-if (!firebase.getApps().length) {
-    const app = firebase.initializeApp(clientCredentials);
-
-    if (typeof document !== 'undefined') {
-        initializeAppCheck(app, {
-            provider: new ReCaptchaV3Provider(
-                '6Lc-tCcdAAAAAEdTLe8olt7t12q9jYnjByn4RO37'
-            ),
-            isTokenAutoRefreshEnabled: true,
-        });
-    }
-}
-
 function MyApp({ Component, pageProps }: AppProps) {
     // hack to fix issue where redirect from stripe
     // loads the root index page instead of the page
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,13 @@
 import type { NextPage } from 'next';
-import React, { useState } from 'react';
-import { getAuth } from '@firebase/auth';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { ClassTimetable } from '../components/ClassTimetable';
-import { ClassDescription } from '../components/ClassDescription';
-import { useClasses } from '../hooks/useClasses';
-import { ClassDescriptionHelp } from '../components/ClassDescriptionHelp';
-import { DanceClass } from '../types/DanceClass';
-import { AnimatePresence } from 'framer-motion';
+import React from 'react';
 
 const Home: NextPage = () => {
-    const [user, userLoading, userError] = useAuthState(getAuth());
-    const [selectedClass, setSelectedClass] = useState<DanceClass>();
-    const [classes, classesLoading, classesError] = useClasses();
-
     return (
         <div className="flex flex-col min-h-full justify-between">
             <main className="md:flex gap-8 px-4 md:px-8 py-4 md:py-10 relative">
-                <div className="md:hidden">
-                    <ClassDescriptionHelp />
-                </div>
-                <ClassTimetable
-                    onRowClick={(row) => setSelectedClass(row)}
-                    selectedClass={selectedClass}
-                />
-
-                <div className="md:flex md:flex-1">
-                    <AnimatePresence>
-                        {selectedClass ? (
-                            <div className="md:static fixed top-0 right-0 left-0 bottom-0 md:z-0 z-10">
-                                <ClassDescription
-                                    danceClass={selectedClass}
-                                    onClose={() => setSelectedClass(undefined)}
-                                />
-                            </div>
-                        ) : (
-                            <div className="hidden md:block w-full">
-                                <ClassDescriptionHelp />
-                            </div>
-                        )}
-                    </AnimatePresence>
-                </div>
+               <div className="text-center">
+                    <h1>Please see more info on Salsa Therapy's <a href="https://www.salsatherapy.co.nz/">main site</a></h1>
+               </div>
             </main>
         </div>
     );
